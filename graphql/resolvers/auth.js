8fe86438cc8f8af4a15e02119354941130ca79a0
Stoner <stoner7969@gmail.com>
2019-02-18 02:05:47 +0800

    feat: Add User Authentication

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const User = require("../../models/user");
 
 module.exports = {
@@ -23,5 +25,30 @@ module.exports = {
       console.log(err);
       throw err;
     }
+  },
+  login: async ({ email, password }) => {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      throw new Error("User does not exist");
+    }
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      throw new Error("Password is incorrect");
+    }
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        email: user.email
+      },
+      "somesupersecretkey",
+      {
+        expiresIn: "1h"
+      }
+    );
+    return {
+      userId: user.id,
+      token: token,
+      tokenExpiration: 1
+    };
   }
 };
--- graphql/resolvers/booking.js
+++ graphql/resolvers/booking.js
@@ -3,7 +3,10 @@ const Booking = require("../../models/booking");
 const { transfromBooking, transformEvent } = require("./merge");
 
 module.exports = {
-  bookings: async () => {
+  bookings: async (args, req) => {
+    if (!req.isAuth) {
+      throw new Error("Unauthenticated!");
+    }
     try {
       const bookings = await Booking.find();
       return bookings.map(booking => {
@@ -13,17 +16,23 @@ module.exports = {
       throw err;
     }
   },
-  bookEvent: async args => {
+  bookEvent: async (args, req) => {
+    if (!req.isAuth) {
+      throw new Error("Unauthenticated!");
+    }
     const fetchedEvent = await Event.findOne({ _id: args.eventId });
     const booking = new Booking({
-      user: "5c5c5ef4f612b71768a42980",
+      user: req.userId,
       event: fetchedEvent
     });
 
     const result = await booking.save();
     return transfromBooking(result);
   },
-  cancelBooking: async args => {
+  cancelBooking: async (args, req) => {
+    if (!req.isAuth) {
+      throw new Error("Unauthenticated!");
+    }
     try {
       const booking = await Booking.findById(args.bookingId).populate("event");
       const event = transformEvent(booking.event);
