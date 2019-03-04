a61927b5cd1c87e5df58d1ae6d8b21b7cffd9602
Stoner <stoner7969@gmail.com>
2019-03-05 00:36:19 +0800

    update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const graphQlResolvers = require("./graphql/resolvers/index");
 const isAuth = require("./middleware/is-auth");
 
 const app = express();
-app.use(express.static('public'));
+
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
@@ -34,7 +34,9 @@ app.use(
 
 mongoose
   .connect(
-    `mongodb+srv://stoner:GfRN9kCX7u10yTLh@cluster0-2vnhb.mongodb.net/events-react-dev?retryWrites=true`
+    `mongodb+srv://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASSWORD
+    }@cluster0-2vnhb.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
   )
   .then(() => {
     app.listen(8000);
